7d993bc8a61085ed6f1c9339542c2d5fc891978a
Frankie Wu <qmwu2000@gmail.com>
2022-05-19 13:22:56 +0800

    remove workspace project

--- workspace/src/test/java/org/unidal/workspace/SetupWorkspace.java
+++
@@ -1,9 +0,0 @@
-package org.unidal.workspace;
-
-import org.junit.runner.RunWith;
-import org.unidal.workspace.junit.WorkspaceRunner;
-
-@RunWith(WorkspaceRunner.class)
-public class SetupWorkspace {
-
-}
